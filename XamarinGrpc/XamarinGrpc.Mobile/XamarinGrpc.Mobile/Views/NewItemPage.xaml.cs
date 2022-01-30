f0dae6cead7e985150a3cddbef1ed46e48b5eea0
michalbarczyk <barczykmt@gmail.com>
2022-01-30 19:54:18 +0100

    prepare solution

---
+++ XamarinGrpc/XamarinGrpc.Mobile/XamarinGrpc.Mobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinGrpc.Mobile.Models;
+using XamarinGrpc.Mobile.ViewModels;
+
+namespace XamarinGrpc.Mobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
